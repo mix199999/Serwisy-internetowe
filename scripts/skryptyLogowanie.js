dc0bf856cfb10bf2cab21e05aacd2ded0bd1d086
Tomek <tpolewczyk19@gmail.com>
2022-12-13 21:50:20 +0100

    Sprawdzanie hasla i emaila za pomoca jsa

--- scripts/skryptyLogowanie.js
+++ scripts/skryptyLogowanie.js
@@ -9,11 +9,64 @@ function reg() {
     document.getElementById("pass").style.display = "block"; // pobranie z dokumentu elementu o ID pass i zmiana jego wyświetlania z none na block
     document.getElementById("email").style.display = "block"; // pobranie z dokumentu elementu o ID email i zmiana jego wyświetlania z none na block
     document.getElementById("subButton").style.display = "block";
-    document.getElementById("subButton").value = "Zarejestruj";
+    document.getElementById("description").style.display = "block";
 
+    document.getElementById("subButton").value = "Zarejestruj";
 
     document.getElementById("acc").style.display = "block";
     document.getElementById("noacc").style.display = "none";
+}
+
+function passwordcheck() {
+
+    let regex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/; // wzór mówiący o tym że hasło musi zawierać dużą literę, cyfrę i być conajmniej 8 znakowe
+    // /^ i $/ to poczatek i koniec "funkcji" regex
+    // ?= oznacza że taki znak musi wystapic w hasle
+    // (?=.*[A-Z]) oznacza że musi zawierać dużą literę
+    // (?=.*\d) oznacza że musi zawierać liczbę  
+    // [A-Za-z\d] mozna wpisac cokolwiek czyli i litery male i duze i liczbe 
+    // {8,} musi miec conajmniej 8 znakow mozna dac ze minimum 8 znakow max 16 wtedy byloby {8,16}
+    let password = document.getElementById("passw").value;
+
+    if(regex.test(password)) {
+
+        document.getElementById("blad").style.display = "none";
+        document.getElementById("subButton").style.pointerEvents = "auto";
+
+    }else if(password == ""){
+
+        document.getElementById("subButton").style.pointerEvents = "none";
+        document.getElementById("blad").style.display = "none";
+    }else {
+
+        document.getElementById("subButton").style.pointerEvents = "none";
+        document.getElementById("blad").style.display = "block";
+
+    }
+}
+
+function emailcheck() {
+    let regex = /^\S+@\S+\.\S+$/;
+    // \S sprawdza czy sa spacje, taby itp
+
+    let email = document.getElementById("em").value;
+
+    if(regex.test(email)) {
+
+        document.getElementById("blademail").style.display = "none";
+        document.getElementById("subButton").style.pointerEvents = "auto";
+
+    }else if(email == ""){
+
+        document.getElementById("subButton").style.pointerEvents = "none";
+        document.getElementById("blademail").style.display = "none";
+    }else {
+
+        document.getElementById("subButton").style.pointerEvents = "none";
+        document.getElementById("blademail").style.display = "block";
+
+    }
+
 }
 
 function log() {
@@ -25,6 +78,7 @@ function log() {
     document.getElementById("pass").style.display = "block"; // pobranie z dokumentu elementu o ID pass i zmiana jego wyświetlania z none na block
     document.getElementById("email").style.display = "none"; // pobranie z dokumentu elementu o ID email i zmiana jego wyświetlania na none
     document.getElementById("subButton").style.display = "block";
+    document.getElementById("description").style.display = "none";
     document.getElementById("subButton").value = "Zaloguj";
 
     document.getElementById("acc").style.display = "none";

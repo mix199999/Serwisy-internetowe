d44a79e76644dd8749561a9855ace5a5e7b3a5bb
Tomek <tpolewczyk19@gmail.com>
2022-12-13 22:11:46 +0100

    Poprawiona wersja

--- scripts/skryptyLogowanie.js
+++ scripts/skryptyLogowanie.js
@@ -1,7 +1,11 @@
 
+let register = 0;
 
 function reg() {
-
+    register = 1;
+    document.getElementById("log").value = "";
+    document.getElementById("passw").value = "";
+    document.getElementById("em").value = "";
     document.getElementById("LoginButton").style.display = "none"; // pobranie z dokumentu elementu o ID LoginButton i zmiana jego wyświetlania na none
     document.getElementById("RegisterButton").style.display = "none"; // pobranie z dokumentu elementu o ID RegisterButton i zmiana jego wyświetlania na none
 
@@ -19,58 +23,67 @@ function reg() {
 
 function passwordcheck() {
 
-    let regex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/; // wzór mówiący o tym że hasło musi zawierać dużą literę, cyfrę i być conajmniej 8 znakowe
-    // /^ i $/ to poczatek i koniec "funkcji" regex
-    // ?= oznacza że taki znak musi wystapic w hasle
-    // (?=.*[A-Z]) oznacza że musi zawierać dużą literę
-    // (?=.*\d) oznacza że musi zawierać liczbę  
-    // [A-Za-z\d] mozna wpisac cokolwiek czyli i litery male i duze i liczbe 
-    // {8,} musi miec conajmniej 8 znakow mozna dac ze minimum 8 znakow max 16 wtedy byloby {8,16}
-    let password = document.getElementById("passw").value;
-
-    if(regex.test(password)) {
-
-        document.getElementById("blad").style.display = "none";
-        document.getElementById("subButton").style.pointerEvents = "auto";
-
-    }else if(password == ""){
-
-        document.getElementById("subButton").style.pointerEvents = "none";
-        document.getElementById("blad").style.display = "none";
-    }else {
-
-        document.getElementById("subButton").style.pointerEvents = "none";
-        document.getElementById("blad").style.display = "block";
-
+    if(register == 1){
+        let regex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/; // wzór mówiący o tym że hasło musi zawierać dużą literę, cyfrę i być conajmniej 8 znakowe
+        // /^ i $/ to poczatek i koniec "funkcji" regex
+        // ?= oznacza że taki znak musi wystapic w hasle
+        // (?=.*[A-Z]) oznacza że musi zawierać dużą literę
+        // (?=.*\d) oznacza że musi zawierać liczbę  
+        // [A-Za-z\d] mozna wpisac cokolwiek czyli i litery male i duze i liczbe 
+        // {8,} musi miec conajmniej 8 znakow mozna dac ze minimum 8 znakow max 16 wtedy byloby {8,16}
+        let password = document.getElementById("passw").value;
+    
+        if(regex.test(password)) {
+    
+            document.getElementById("blad").style.display = "none";
+            document.getElementById("subButton").style.pointerEvents = "auto";
+    
+        }else if(password == ""){
+    
+            document.getElementById("subButton").style.pointerEvents = "none";
+            document.getElementById("blad").style.display = "none";
+        }else {
+    
+            document.getElementById("subButton").style.pointerEvents = "none";
+            document.getElementById("blad").style.display = "block";
+    
+        }
     }
+   
 }
 
 function emailcheck() {
-    let regex = /^\S+@\S+\.\S+$/;
-    // \S sprawdza czy sa spacje, taby itp
-
-    let email = document.getElementById("em").value;
-
-    if(regex.test(email)) {
-
-        document.getElementById("blademail").style.display = "none";
-        document.getElementById("subButton").style.pointerEvents = "auto";
-
-    }else if(email == ""){
-
-        document.getElementById("subButton").style.pointerEvents = "none";
-        document.getElementById("blademail").style.display = "none";
-    }else {
-
-        document.getElementById("subButton").style.pointerEvents = "none";
-        document.getElementById("blademail").style.display = "block";
-
+    if(register == 1) {
+        let regex = /^\S+@\S+\.\S+$/;
+        // \S sprawdza czy sa spacje, taby itp
+    
+        let email = document.getElementById("em").value;
+    
+        if(regex.test(email)) {
+    
+            document.getElementById("blademail").style.display = "none";
+            document.getElementById("subButton").style.pointerEvents = "auto";
+    
+        }else if(email == ""){
+    
+            document.getElementById("subButton").style.pointerEvents = "none";
+            document.getElementById("blademail").style.display = "none";
+        }else {
+    
+            document.getElementById("subButton").style.pointerEvents = "none";
+            document.getElementById("blademail").style.display = "block";
+    
+        }
     }
+    
 
 }
 
 function log() {
-
+    register = 0;
+    document.getElementById("log").value = "";
+    document.getElementById("passw").value = "";
+    document.getElementById("subButton").style.pointerEvents = "auto";
     document.getElementById("LoginButton").style.display = "none"; // pobranie z dokumentu elementu o ID LoginButton i zmiana jego wyświetlania na none
     document.getElementById("RegisterButton").style.display = "none"; // pobranie z dokumentu elementu o ID RegisterButton i zmiana jego wyświetlania na none
 

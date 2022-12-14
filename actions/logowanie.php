c33f5e1b04388f93c55ca9707f201a5c55dbdffd
Michau99 <mix1999a@gmail.com>
2022-12-14 17:38:46 +0100

    naprawa

--- actions/logowanie.php
+++ actions/logowanie.php
@@ -12,10 +12,8 @@ $user = new User($db);
 
 
 try {
-   
-       
 
-        if(isset($_POST["login"])  && isset($_POST["passwd"]))
+        if(isset($_POST["login"])  && isset($_POST["passwd"])&& ($_POST["email"])== '')
         {	
             $user = new User($db);
             $user->login = $_POST["login"];
@@ -25,21 +23,17 @@ try {
 
                 if($_SESSION["user_type"] == 1)
                 {
-
                     header('Location: index.php?action=StartPage');
                 }
 
                 else if ($_SESSION["user_type"] == 2)
 
                 {
-
                     header('Location: index.php?action=StartPage');
                 }
 
                 else if ($_SESSION["user_type"] == 3)
-
                 {
-
                     header('Location: index.php?action=StartPage');
                 }
                //todo jesli inne role
@@ -49,6 +43,24 @@ try {
             } else {
                 $loginMessage = 'Invalid login! Please try again.';
             }
+        }
+
+
+        else if(isset($_POST["login"])  && isset($_POST["passwd"])&& $_POST["email"] != '')
+        {
+            $user = new User($db);
+            $user->login = $_POST["login"];
+            $user->id_priv= $_POST["id_priv"];
+            $user->passwd= $_POST["passwd"];
+            $user->email= $_POST["email"];
+
+            if($user->addUser())
+            {
+                header('Location: index.php?action=newUser');
+                echo $_SESSION["user_type"];
+
+            }
+
         }
        
 

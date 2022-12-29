bcbf8a9a94ba0f4523c8c36e65433d6f8efae65b
TurboKebabNowok <nowok11.2012@gmail.com>
2022-12-29 14:13:03 +0100

    reports 3, kolejne poprawki

--- actions/changePrivil.php
+++ actions/changePrivil.php
@@ -1,9 +1,13 @@
 <?php
 echo 'privil';
 
+if(isset($_POST['reason']) and isset($_POST['title']) ){
+
         //$userId = $_POST['userId'];
         $reason = $_POST['reason'];
         $title = $_POST['title'];
+        
+}
         //wszytko jest dowiedziec sie jakie tabele itp i co gdzie jest zapisane, tak samo mozna uzytkownika tylko łatwiej
         // videoID, reason, title, userId, status?
         

33b00038bdc562c3bf265295bd35f75196b9573a
chalmi23 <michalekxdpl23@gmail.com>
2022-12-30 12:44:37 +0100

    Wygląd strony watch oraz usuniecie watchVideo

--- actions/watch.php
+++ actions/watch.php
@@ -1,34 +1,16 @@
 <?php
 include_once 'conf/connDB.php';
 include_once 'class/Video.php';
+include_once 'class/Users.php';
 
+$database = new Database();
+$db = $database->getConnection();
+$user = new User($db);
 
-if(isset($_GET['v'])){
+$user->id_user = $_SESSION["id_user"];
+$user->getUserInfo();
 
-    $database = new Database();
-    $conn = $database->getConnection();
-    $video = new Video($conn, $_GET['v']);
-    $type = null;
+$urlTable = Video::getVideosWithUserTags($user->login, $db);
+$currentIndex = isset($_GET['index']) ? intval($_GET['index']) : 0; //index filmu przy wyswietlaniu ich na stronie
 
-
-    if($video->completeFromDb() < 0){ //To nie zadziała do końca na razie, bo musi mi Michał zmienić trochę bazę. Jakbyś potrzebował jakichś danych których tam nie ma to mnie wołaj
-        echo "Błędny kod filmu";
-    }
-    else{
-        if($video->getExtension() == "url"){
-            $type = "link";
-            //kod od Michała Nowaka
-        }
-        else{
-            $type = "server";
-            //normalne wyświetlanie
-        }
-        //tutaj możesz robić to zgłaszanie, w $video będą w polach wszysstkie dane więc z nich korzystać możesz
-    }
-
-
-}
-else{
-    echo "Aby wyświetlić wideo należy podać kod";
-}
 ?>

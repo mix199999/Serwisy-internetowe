7181df2fa7971fbd9b0a66a1639514448ffd19e6
TurboKebabNowok <nowok11.2012@gmail.com>
2023-01-02 13:23:18 +0100

    reports, ajaxem

--- actions/changePrivil.php
+++ actions/changePrivil.php
@@ -1,16 +1,18 @@
 <?php
-echo 'privil';
+require_once $_SERVER['DOCUMENT_ROOT'].'/conf/connDB.php';
 
-if(isset($_POST['reason']) and isset($_POST['title']) ){
+$database = new Database();
+$db = $database->getConnection();
 
-        //$userId = $_POST['userId'];
-        $reason = $_POST['reason'];
-        $title = $_POST['title'];
-        
+if(isset($_POST['reason3']) and isset($_POST['title3']) ){
+
+        $id_user=$_SESSION["id_user"];
+        $reason = $_POST['reason3'];
+        $title = $_POST['title3'];
+
+        $stmt = $db->prepare("INSERT INTO privilege_change_request (case_id, user_id, case_status, user_message, admin_message, title) VALUES (null, :user_id, 1, :user_message, null, :title)");
+        $stmt->bindParam(':user_id', $id_user);
+        $stmt->bindParam(':user_message', $reason);
+        $stmt->bindParam(':title', $title);
+        $stmt->execute();
 }
-        //wszytko jest dowiedziec sie jakie tabele itp i co gdzie jest zapisane, tak samo mozna uzytkownika tylko łatwiej
-        // videoID, reason, title, userId, status?
-        
-        //$stmt = $db->prepare("INSERT INTO reports (videoId, reason, title) VALUES (:videoId, :reason, :title)");
-        //$stmt->bindParam(':videoId', $videoId);
-        //itd...
